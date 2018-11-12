a015a24f9c98687136464e516fd43625ab4913e0
Bibesh KC <bibesh.kc@selu.edu>
2018-11-12 15:28:55 -0600

    Update longestCommonSequence.java
    Fixed the issue with the repetition

--- longestCommonSequence.java
+++ longestCommonSequence.java
@@ -39,7 +39,7 @@ public class longestCommonSequence {
 		for(int i = 1; i<lcs.length;i++) {
 			for(int j = 1; j <lcs[i].length;j++) {
 				if(firstArr[i-1] == secondArr[j-1]) {
-					lcs[i][j] = 1 + Integer.max(lcs[i-1][j-1], Integer.max(lcs[i-1][j], lcs[i][j-1]));
+					lcs[i][j] = 1 + lcs[i-1][j-1];
 				} else {
 					lcs[i][j] = Integer.max(lcs[i-1][j-1], Integer.max(lcs[i-1][j], lcs[i][j-1]));
 				}
